edefbdb970ac9ddf4e1e97687aa79762826d5c36
Mistire <mdiwithgod@gmail.com>
2025-08-07 13:48:39 +0300

    feat: Implement API views to manage listings and bookings, and ensure the endpoints are documented with Swagger

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -15,23 +15,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
-from rest_framework import permissions
+from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="ALX Travel API",
-      default_version='v1',
-      description="API documentation for the ALX Travel App",
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Travel API",
+        default_version='v1',
+        description="API documentation for Listings and Bookings",
+    ),
+    public=True,
+    permission_classes=[AllowAny],
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/', include('listings.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
 
